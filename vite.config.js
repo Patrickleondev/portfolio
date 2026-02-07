325bea439568a303fa661b44578f89da0fc44855
Patrickleondev <gkpl0010@gmail.com>
2026-02-07 11:00:29 +0000

    feat: multi-page structure, footer, badge, and carousels

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/portfolio/', // Repo name is 'portfolio'
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                about: 'about.html',
+                expertise: 'expertise.html',
+                projects: 'projects.html',
+            },
+        },
+    },
 })
